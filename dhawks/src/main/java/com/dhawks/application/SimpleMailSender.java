c0ff148748782fe946e3fea8e3889506531657ec
Nitin Bisht <3498georgian@gmail.com>
2022-01-10 12:41:43 +0530

    Added Subject and Body File

--- dhawks/src/main/java/com/dhawks/application/SimpleMailSender.java
+++ dhawks/src/main/java/com/dhawks/application/SimpleMailSender.java
@@ -2,24 +2,50 @@ package com.dhawks.application;
 
 import com.dhawks.application.service.EmailSenderService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
 @SpringBootApplication
 public class SimpleMailSender {
-	@Autowired
-	private EmailSenderService emailSenderService;
 
-	public static void main(String[] args) {
-		SpringApplication.run(SimpleMailSender.class, args);
-	}
+    @Value("${mail.location.subject.text}")
+    private String subjectFileLocation;
+
+    @Value("${mail.location.body.text}")
+    private String bodyFileLocation;
+
+    @Autowired
+    private EmailSenderService emailSenderService;
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleMailSender.class, args);
+    }
 
 
-	@EventListener(ApplicationReadyEvent.class)
-	public void sendEmail(){
-		emailSenderService.sendEmail("3498georgian@gmail.com","Try"," Wassup nitin bisht");
-	}
+    @EventListener(ApplicationReadyEvent.class)
+    public void sendEmail() {
+        emailSenderService.sendEmail("3498georgian@gmail.com",
+                initMessageString(subjectFileLocation), initMessageString(bodyFileLocation));
+    }
+
+    private String initMessageString(String fileLocation) {
+        Path path = Paths.get(fileLocation);
+        StringBuilder builder = new StringBuilder();
+        try (Stream<String> lines = Files.lines(path)) {
+            lines.forEach(line -> builder.append(line));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return builder.toString();
+    }
 
 }
